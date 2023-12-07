54c8e0f5b3b07559c027adc3f9aa8e0d36d35fbf
maxence.ginoux <maxence.ginoux@laplateforme.io>
2023-12-07 11:59:44 +0100

    modif

--- main.py
+++ main.py
@@ -1,74 +1,73 @@
-import pygame
+from tkinter import *
 from pygame import mixer
-import tkinter
+import os
+import pygame
 
-
-pygame.init()
+# Initialisation
 mixer.init()
+pygame.init()
 
-pygame.display.set_caption("playerMusic")
-fenetre = pygame.display.set_mode((500, 400))
-
-# LESMUSIQUES
-piste1 = pygame.mixer.music.load("media/unesourisverte.mp3")
-piste2 = pygame.mixer.music.load("media/lepetitbonhomme.mp3")
-playlist = (piste1, piste2)
-
-# LES IMAGES
-fond = pygame.image.load("media/image2.webp")
-fond = fond.convert()
+root = Tk()
+root.title("playerMusic")
 
 
-# PARAMETRES DE DEPART
-running = True
-volume = 50
-
-# CORPS DU PROGRAMME
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: # pygame.QUIT pour fermer la fenêtre
-            running = False
-            quit()
-        elif event.type == pygame.MOUSEBUTTONDOWN: # Si le clic de la souris est dans la zone du bouton
-            if 100 <= event.pos[0] <= 300 and 100 <= event.pos[1] <= 150: # Augmentation du volume
-                if volume < 100:  # Limite le volume à 100
-                    volume += 10  # Augmentation de 10 unités
-    fenetre.blit(fond, (0,0)) # OU fenetre.fill((125,235,110)) # POUR LA COULEUR DE FOND
-    pygame.display.flip() # POUR AFFICHER LE FOND
-
-
-# LES FONCTIONS
-def bouton_play():
-    mixer.music.load(playlist)
+def play_musique():
+    currentsong = playlist.get(ACTIVE)
+    mixer.music.load(currentsong)
     mixer.music.play()
 
+def pause_musique():
+    mixer.music.pause()
 
-def bouton_stop():
+def stop_musique():
     mixer.music.stop()
 
-def bouton_pause():
-    mixer.music.pause()
-                    
-def bouton_resume():
+def resume_musique():
     mixer.music.unpause()
 
-#def bouton_suivant():
+def suivant_musique():
+    current_index = playlist.curselection()
+    next_index = current_index[0] + 1 if current_index else 0
+    if next_index < playlist.size():
+        playlist.selection_clear(0, END)
+        playlist.selection_set(next_index)
+
+def precedent_musique():
+    current_index = playlist.curselection()
+    prev_index = current_index[0] - 1 if current_index else 0
+    if prev_index >= 0:
+        playlist.selection_clear(0, END)
+        playlist.selection_set(prev_index)
+
+# Liste des chansons
+playlist = Listbox(root, selectmode=SINGLE, bg="black", fg="white", font=('arial', 15), width=40)
+playlist.grid(columnspan=5)
+
+# Chargement des chansons du répertoire courant
+os.chdir("C:/Users/ginma/Documents/La Plateforme/playerMusic/playerMusic/musique/")
+songs = os.listdir()
+
+for s in songs:
+    playlist.insert(END, s)
+
+# Boutons interactifs
+precedent_bouton = Button(root, text="précédent", command=precedent_musique)
+precedent_bouton.grid(row=1, column=0)
+
+play_bouton = Button(root, text="Play", command=play_musique)
+play_bouton.grid(row=1, column=1)
+
+pause_bouton = Button(root, text="Pause", command=pause_musique)
+pause_bouton.grid(row=1, column=2)
+
+Resume_bouton = Button(root, text="Resume", command=resume_musique)
+Resume_bouton.grid(row=1, column=3)
+
+stop_bouton = Button(root, text="Stop", command=stop_musique)
+stop_bouton.grid(row=1, column=4)
+
+suivant_bouton = Button(root, text="suivant", command=suivant_musique)
+suivant_bouton.grid(row=1, column=5)
 
 
-#def bouton_precedent():
-
-
-#def bouton_aleatoire():
-
-
-#def bouton_repeat():
-
-
-def bouton_volumeplus():
-    font = pygame.font.Font(None, 36)
-    texte = font.render("Augmenter le Volume", True)
-    pygame.draw.rect(fenetre(100, 100, 200, 50))
-    # fenetre.blit(texte, (110, 110))
-
-
-#def bouton_volumemoins():
+mainloop()
